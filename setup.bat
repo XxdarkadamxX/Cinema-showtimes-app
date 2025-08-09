310fbe8b8aced2a49468e9f4cb547a1ece515ffc
XxdarkadamxX <adamharzi@gmail.com>
2025-08-09 15:41:13 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Creating virtual environment...
+python -m venv venv
+
+echo Activating virtual environment...
+call venv\Scripts\activate
+
+echo Installing dependencies...
+pip install -r requirements.txt
+
+echo Setup complete!
+echo To activate the virtual environment, run: venv\Scripts\activate
+echo To run the script: python ugc_films_fetcher.py 
